19a81e148b8201ba7d7684ba12df00f1bae1c540
Farwa Rizvi <farwamehdi3@gmail.com>
2025-02-15 19:35:15 +0100

    domloaded added to correct the change of background color

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-let button = document.querySelector(".netflix-button");
-let body = document.body;
-let interacted = false;
-let animationDone = false;
+document.addEventListener("DOMContentLoaded", function () {
+  let button = document.querySelector(".netflix-button");
+  let body = document.body;
+  let interacted = false;
+  let animationDone = false;
 
-function startAnimation() {
-  button.classList.add("start");
-  console.log(startAnimation);
+  function startAnimation() {
+    button.classList.add("start");
+    //   console.log(startAnimation);
 
-  setTimeout(() => {
-    animationDone = true;
-    if (!interacted) {
-      triggerAction(); // Hvis ingen interaktion sker, skift baggrund
-    }
-  }, 3000); // 3 sekunder til animationen fuldføres
-}
+    setTimeout(() => {
+      animationDone = true;
+      if (!interacted) {
+        triggerAction(); // Hvis ingen interaktion sker, skift baggrund
+      }
+    }, 3000); // 3 sekunder til animationen fuldføres
+  }
 
-function triggerAction() {
-  body.style.backgroundColor = "#e50914"; // Skifter baggrund til Netflix-rød
-  button.style.display = "none"; // Skjuler knappen
-  console.log(triggerAction);
-}
+  function triggerAction() {
+    body.style.backgroundColor = "#e50914"; // Skifter baggrund til Netflix-rød
+    button.style.display = "none"; // Skjuler knappen
+    //   console.log(triggerAction);
+  }
 
-function handleInteraction() {
-  interacted = true;
-  console.log(handleInteraction);
-}
+  function handleInteraction() {
+    interacted = true;
+    //   console.log(handleInteraction);
+  }
 
-// Lyt efter interaktioner
-button.addEventListener("click", triggerAction);
-window.addEventListener("mousemove", handleInteraction);
-window.addEventListener("keydown", handleInteraction);
+  // Lyt efter interaktioner
+  button.addEventListener("click", triggerAction);
+  window.addEventListener("mousemove", handleInteraction);
+  window.addEventListener("keydown", handleInteraction);
 
-// Start animationen, når siden loader
-startAnimation();
+  // Start animationen, når siden loader
+  startAnimation();
+});
